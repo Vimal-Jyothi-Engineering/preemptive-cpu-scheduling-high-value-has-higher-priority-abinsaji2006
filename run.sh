7ae0494fecbbed0b65a28985098967b365aec32e
abinsaji2006 <abins4lab@gmail.com>
2026-03-11 10:50:11 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
+# Generate Makefile so "make test" works
+cat > Makefile << 'EOF'
+CC = gcc
+
+priority: priority.c
+	@$(CC) priority.c -o priority
+
+test: priority.c
+	@$(CC) priority.c -o priority
+	@./priority
+
+clean:
+	@rm -f priority
+EOF
+
 gcc priority.c -o priority
 ./priority
