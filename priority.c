ed77a23f7ceb87f6f2d559362f564a44861844f2
abinsaji2006 <abins4lab@gmail.com>
2026-03-11 12:19:05 +0530

    Update 15priority.c

--- priority.c
+++ priority.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 #define MAX 100
 
 typedef struct {
-    char name[20];
+    char name[10];
     int arrival;
     int burst;
     int priority;
-    int remaining;
     int finish;
     int waiting;
     int turnaround;
@@ -19,55 +17,18 @@ int main() {
 
     Process p[MAX];
     int n = 0;
-    char first[20];
 
-    if(scanf("%s", first) != 1)
-        return 0;
-
-    if(first[0] >= '0' && first[0] <= '9') {
-
-        int count = atoi(first);
-
-        for(int i = 0; i < count; i++) {
-            scanf("%s %d %d %d",
-                  p[n].name,
-                  &p[n].arrival,
-                  &p[n].burst,
-                  &p[n].priority);
-
-            p[n].remaining = p[n].burst;
-            n++;
-        }
-    }
-    else {
-
-        strcpy(p[n].name, first);
-
-        scanf("%d %d %d",
-              &p[n].arrival,
-              &p[n].burst,
-              &p[n].priority);
-
-        p[n].remaining = p[n].burst;
+    while (scanf("%s %d %d %d", p[n].name,
+           &p[n].arrival, &p[n].burst, &p[n].priority) == 4) {
         n++;
-
-        while(scanf("%s %d %d %d",
-                    p[n].name,
-                    &p[n].arrival,
-                    &p[n].burst,
-                    &p[n].priority) == 4) {
-
-            p[n].remaining = p[n].burst;
-            n++;
-        }
     }
 
-    int time = 0;
     int completed = 0;
+    int time = 0;
     int done[MAX] = {0};
 
     int min_arrival = p[0].arrival;
-    for(int i = 1; i < n; i++)
+    for(int i=1;i<n;i++)
         if(p[i].arrival < min_arrival)
             min_arrival = p[i].arrival;
 
@@ -76,19 +37,19 @@ int main() {
     while(completed < n) {
 
         int idx = -1;
-        int best_priority = -1;
+        int highest = -1;
 
-        for(int i = 0; i < n; i++) {
+        for(int i=0;i<n;i++) {
 
             if(!done[i] && p[i].arrival <= time) {
 
-                if(p[i].priority > best_priority) {
-                    best_priority = p[i].priority;
+                if(p[i].priority > highest) {
+                    highest = p[i].priority;
                     idx = i;
                 }
-                else if(p[i].priority == best_priority) {
 
-                    if(idx == -1 || p[i].arrival < p[idx].arrival)
+                else if(p[i].priority == highest) {
+                    if(p[i].arrival < p[idx].arrival)
                         idx = i;
                 }
             }
@@ -99,42 +60,37 @@ int main() {
             continue;
         }
 
-        p[idx].remaining--;
-        time++;
+        time += p[idx].burst;
 
-        if(p[idx].remaining == 0) {
+        p[idx].finish = time;
 
-            p[idx].finish = time;
+        p[idx].turnaround =
+        p[idx].finish - p[idx].arrival;
 
-            p[idx].turnaround =
-                p[idx].finish - p[idx].arrival;
+        p[idx].waiting =
+        p[idx].turnaround - p[idx].burst;
 
-            p[idx].waiting =
-                p[idx].turnaround - p[idx].burst;
-
-            done[idx] = 1;
-            completed++;
-        }
+        done[idx] = 1;
+        completed++;
     }
 
     printf("Waiting Time:\n");
-    for(int i = 0; i < n; i++)
+    for(int i=0;i<n;i++)
         printf("%s %d\n", p[i].name, p[i].waiting);
 
     printf("Turnaround Time:\n");
-    for(int i = 0; i < n; i++)
+    for(int i=0;i<n;i++)
         printf("%s %d\n", p[i].name, p[i].turnaround);
 
-    double total_w = 0;
-    double total_t = 0;
+    double total_w=0,total_t=0;
 
-    for(int i = 0; i < n; i++) {
-        total_w += p[i].waiting;
-        total_t += p[i].turnaround;
+    for(int i=0;i<n;i++){
+        total_w+=p[i].waiting;
+        total_t+=p[i].turnaround;
     }
 
-    printf("Average Waiting Time: %.2f\n", total_w / n);
-    printf("Average Turnaround Time: %.2f\n", total_t / n);
+    printf("Average Waiting Time: %.2f\n", total_w/n);
+    printf("Average Turnaround Time: %.2f\n", total_t/n);
 
     return 0;
 }
