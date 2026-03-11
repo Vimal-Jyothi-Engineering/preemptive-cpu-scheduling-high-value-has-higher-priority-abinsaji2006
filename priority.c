575f3a45a27046df7b5f87c04fcfcf926a468c11
abinsaji2006 <abins4lab@gmail.com>
2026-03-11 12:09:03 +0530

    Update 13priority.c

--- priority.c
+++ priority.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define MAX 100
 
 typedef struct {
-    char name[10];
+    char name[20];
     int arrival;
     int burst;
     int priority;
@@ -18,75 +19,111 @@ int main() {
 
     Process p[MAX];
     int n = 0;
+    char first[20];
 
-    while (scanf("%s %d %d %d", p[n].name,
-           &p[n].arrival, &p[n].burst, &p[n].priority) == 4) {
+    if(scanf("%s", first) != 1) return 0;
+
+    if(first[0] >= '0' && first[0] <= '9') {
+        int count = atoi(first);
+
+        for(int i=0;i<count;i++) {
+            scanf("%s %d %d %d",
+                  p[n].name,
+                  &p[n].arrival,
+                  &p[n].burst,
+                  &p[n].priority);
+
+            p[n].remaining = p[n].burst;
+            n++;
+        }
+    }
+    else {
+        strcpy(p[n].name, first);
+        scanf("%d %d %d",
+              &p[n].arrival,
+              &p[n].burst,
+              &p[n].priority);
 
         p[n].remaining = p[n].burst;
         n++;
+
+        while(scanf("%s %d %d %d",
+              p[n].name,
+              &p[n].arrival,
+              &p[n].burst,
+              &p[n].priority) == 4) {
+
+            p[n].remaining = p[n].burst;
+            n++;
+        }
     }
 
-    int time = 0;
+    int finished[MAX] = {0};
     int completed = 0;
-    int done[MAX] = {0};
+    int time = 0;
 
-    while (completed < n) {
+    while(completed < n) {
 
         int idx = -1;
-        int highest = -1;
+        int best_priority = -1;
 
-        for (int i = 0; i < n; i++) {
+        for(int i=0;i<n;i++) {
 
-            if (!done[i] && p[i].arrival <= time) {
+            if(!finished[i] && p[i].arrival <= time) {
 
-                if (p[i].priority > highest) {
-                    highest = p[i].priority;
+                if(p[i].priority > best_priority) {
+                    best_priority = p[i].priority;
                     idx = i;
                 }
-
-                else if (p[i].priority == highest) {
-                    if (p[i].arrival < p[idx].arrival)
+                else if(p[i].priority == best_priority) {
+                    if(p[i].arrival < p[idx].arrival)
                         idx = i;
                 }
             }
         }
 
-        if (idx == -1) {
+        if(idx == -1) {
             time++;
-            continue;
         }
+        else {
 
-        p[idx].remaining--;
-        time++;
+            p[idx].remaining--;
+            time++;
 
-        if (p[idx].remaining == 0) {
+            if(p[idx].remaining == 0) {
 
-            p[idx].finish = time;
-            p[idx].turnaround = p[idx].finish - p[idx].arrival;
-            p[idx].waiting = p[idx].turnaround - p[idx].burst;
+                p[idx].finish = time;
 
-            done[idx] = 1;
-            completed++;
+                p[idx].turnaround =
+                    p[idx].finish - p[idx].arrival;
+
+                p[idx].waiting =
+                    p[idx].turnaround - p[idx].burst;
+
+                finished[idx] = 1;
+                completed++;
+            }
         }
     }
 
     printf("Waiting Time:\n");
-    for (int i = 0; i < n; i++)
+    for(int i=0;i<n;i++)
         printf("%s %d\n", p[i].name, p[i].waiting);
 
     printf("Turnaround Time:\n");
-    for (int i = 0; i < n; i++)
+    for(int i=0;i<n;i++)
         printf("%s %d\n", p[i].name, p[i].turnaround);
 
-    double avg_w = 0, avg_t = 0;
+    double total_wt = 0;
+    double total_tat = 0;
 
-    for (int i = 0; i < n; i++) {
-        avg_w += p[i].waiting;
-        avg_t += p[i].turnaround;
+    for(int i=0;i<n;i++) {
+        total_wt += p[i].waiting;
+        total_tat += p[i].turnaround;
     }
 
-    printf("Average Waiting Time: %.2f\n", avg_w / n);
-    printf("Average Turnaround Time: %.2f\n", avg_t / n);
+    printf("Average Waiting Time: %.2f\n", total_wt/n);
+    printf("Average Turnaround Time: %.2f\n", total_tat/n);
 
     return 0;
 }
